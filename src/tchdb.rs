9c9926c5a60761bfafac55d379016ee2884004cc
ckxkexing <1585799755@qq.com>
2022-12-12 14:26:51 +0800

    :white_check_mark: use length to get from tch

--- src/tchdb.rs
+++ src/tchdb.rs
@@ -40,22 +40,37 @@ impl TCH {
         if res { Some(res) }
         else { None }
     }
+    pub fn tchdbtune(&self, bnum: i64, apow: i8, fpow: i8, opts: u8) -> bool{
+        unsafe{
+            tchdbtune(self.0, bnum , apow , fpow , opts)
+        }
+    }
+    pub fn tchdbsetcache(&self, rcnum: i32){
+        unsafe {
+            tchdbsetcache(self.0, rcnum);
+        }
+    }
     pub fn tchdbput2(&self, key:CString, value:CString) -> bool{
         unsafe {tchdbput2(self.0, key.as_ptr(), value.as_ptr()) }
     }
-    pub fn tchdbget2(&self, key:CString) -> String{
-       let v2 = unsafe {tchdbget2(self.0, key.as_ptr())};
-       let v2_cstr = unsafe {CStr::from_ptr(v2)};
-       String::from(v2_cstr.to_str().unwrap())
+    pub fn tchdbget2(&self, key:CString) -> Option<String>{
+        let v2 = unsafe {tchdbget2(self.0, key.as_ptr())};
+        if v2.is_null(){
+            return None;
+        }
+        let v2_cstr = unsafe {CStr::from_ptr(v2)};
+        Some(String::from(v2_cstr.to_str().unwrap()))
     }
     pub fn tchdbdel(&self) {
         unsafe {tchdbdel(self.0) }
     }
+    pub fn tchdbclose(&self) -> bool {unsafe {tchdbclose(self.0)}}
 }
 
 #[cfg(test)]
 mod tests {
     use std::ffi::{CStr, CString};
+    use std::slice;
     use libc::{c_char, c_int};
     use crate::tchdb::*;
     fn str2cstr(str:&str) -> CString {
@@ -78,17 +93,21 @@ mod tests {
             let tchdb = tchdbnew();
 
             // let cpath = str2cchar("./casket.tch");
-            let cpath = str2cstr("./casket.tch");
+            let cpath = str2cstr("./casket2.tch");
 
+            let res = tchdbtune(tchdb, 16777213, -1, -1, 1);
+            assert_eq!(res, true);
+            let res = tchdbsetcache(tchdb, 100000);
+            assert_eq!(res, true);
             let res = tchdbopen(tchdb, cpath.as_ptr(), (HDBOWRITER | HDBOCREAT) as c_int);
             assert_eq!(HDBOWRITER, 2);
             assert_eq!(res, true);
 
-            let err = !tchdbput2(tchdb, str2cstr("foo").as_ptr(), str2cstr("value").as_ptr()) ||
-                !tchdbput2(tchdb, str2cstr("bar").as_ptr(), str2cstr("step").as_ptr()) ||
-                !tchdbput2(tchdb, str2cstr("baz").as_ptr(), str2cstr("jump").as_ptr());
+            // let err = !tchdbput2(tchdb, str2cstr("foo").as_ptr(), str2cstr("value").as_ptr()) ||
+            //     !tchdbput2(tchdb, str2cstr("bar").as_ptr(), str2cstr("step").as_ptr()) ||
+            //     !tchdbput2(tchdb, str2cstr("baz").as_ptr(), str2cstr("jump").as_ptr());
 
-            assert_eq!(err, false);
+            // assert_eq!(err, false);
 
             // Query
             let v2 = tchdbget2(tchdb, str2cstr("foo").as_ptr());
@@ -102,8 +121,8 @@ mod tests {
             while let key = tchdbiternext2(tchdb){
                 if key.is_null(){break;}
                 let v2 = tchdbget2(tchdb, key);
-                let v2_cstr = CStr::from_ptr(v2);
-                println!("traverse :: {:?}:{:?}", CStr::from_ptr(key), v2_cstr);
+                let v2_cstr = CStr::from_ptr(v2).to_str().unwrap();
+                println!("traverse1 :: {:?}:{:?}", CStr::from_ptr(key).to_str().unwrap(), v2_cstr);
             }
             /*
                 output:
@@ -111,7 +130,7 @@ mod tests {
                     traverse :: "bar":"step"
                     traverse :: "baz":"jump"
             */
-            tchdbdel(tchdb);
+            tchdbclose(tchdb);
 
             /*
             *   Start to test Read
@@ -120,7 +139,7 @@ mod tests {
             let tchdb = tchdbnew();
 
             // let cpath = str2cchar("./casket.tch");
-            let cpath = str2cstr("./casket.tch");
+            let cpath = str2cstr("./casket2.tch");
 
             let res = tchdbopen(tchdb, cpath.as_ptr(), HDBOREADER as c_int);
 
@@ -136,11 +155,13 @@ mod tests {
             while let key = tchdbiternext2(tchdb){
                 if key.is_null(){break;}
                 let v2 = tchdbget2(tchdb, key);
+                // let v2 = tchdbget(tchdb, key, sz);
                 let v2_cstr = CStr::from_ptr(v2);
-                println!("traverse :: {:?}:{:?}", CStr::from_ptr(key), v2_cstr);
+
+                println!("traverse2 :: {:?}:{:?}", CStr::from_ptr(key), v2_cstr);
             }
 
-            tchdbdel(tchdb);
+            tchdbclose(tchdb);
         }
     }
 
@@ -148,11 +169,13 @@ mod tests {
     // chenkx: Test custom Struct and Methods
     fn test_TCH(){
         let db = TCH::tchdbnew().unwrap();
-        let path = str2cstr("./casket2.tch");
+        let path = str2cstr("./casket.tch");
+        db.tchdbtune( 16777213, -1, -1, 1);
+        db.tchdbsetcache(100000);
         db.tchdbopen(path, (HDBOWRITER | HDBOCREAT) as c_int).unwrap();
-        db.tchdbput2(str2cstr("foo"), str2cstr("value"));
+        // db.tchdbput2(str2cstr("foo"), str2cstr("value"));
         let v = db.tchdbget2(str2cstr("foo"));
         assert_eq!(v, String::from("value"));
-        db.tchdbdel();
+        db.tchdbclose();
     }
 }
